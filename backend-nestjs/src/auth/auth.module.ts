0b75d3209a5fc5c0004388f361599abd020a3de1
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-07-19 16:41:36 +0700

    covert time by lib ms

--- backend-nestjs/src/auth/auth.module.ts
+++ backend-nestjs/src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { LocalStrategy } from './passport/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './passport/jwt.strategy';
+import ms from 'ms';
 
 @Module({
   imports: [
@@ -14,15 +15,15 @@ import { JwtStrategy } from './passport/jwt.strategy';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+        secret: configService.get<string>('JWT_ACCESS_TOKEN'),
         signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRE'),
+          expiresIn: ms(configService.get<string>('JWT_ACCESS_EXPIRE')),
         },
       }),
       inject: [ConfigService],
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
-  exports: [AuthService]
+  exports: [AuthService],
 })
 export class AuthModule {}
