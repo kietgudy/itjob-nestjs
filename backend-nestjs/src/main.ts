38f66b2143cb407c9235a834105c9d9332ef10b4
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-07-20 14:44:08 +0700

    fix cors

--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -11,11 +11,17 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   const reflector = app.get(Reflector);
-  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  // app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
+  //config cors
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
