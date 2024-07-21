4c3f46c5ef74bfabcee70120eed4316c6c62b1a7
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-07-21 22:41:22 +0700

    config versioning api

--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 require('dotenv').config();
@@ -23,6 +23,13 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+  });
+
+  //config versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2']
   });
   await app.listen(configService.get<string>('PORT'));
 }
