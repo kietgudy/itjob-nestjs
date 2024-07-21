5f1465c1f1a302618d9a6829d81a21e58764151c
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-07-21 23:04:51 +0700

    update schema user

--- backend-nestjs/src/users/schemas/user.schema.ts
+++ backend-nestjs/src/users/schemas/user.schema.ts
@@ -1,22 +1,46 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema({timestamps: true})
+@Schema({ timestamps: true })
 export class User {
-  @Prop({required: true})
+  @Prop({ required: true })
   email: string;
-  @Prop({required: true})
+  @Prop({ required: true })
   password: string;
-  @Prop({required: true})
+  @Prop({ required: true })
   name: string;
   @Prop()
   age: number;
   @Prop()
-  phone: string;
+  gender: string;
   @Prop()
   address: string;
+  @Prop()
+  role: string;
+  @Prop()
+  refreshToken: string;
+  @Prop({ type: Object })
+  campany: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+  };
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
   @Prop()
   createdAt: Date;
   @Prop()
