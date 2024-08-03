189e7999c1b59fe364a4b72e2a25a93097d42906
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-03 17:40:25 +0700

    create dto update resume

--- backend-nestjs/src/resumes/dto/update-resume.dto.ts
+++ backend-nestjs/src/resumes/dto/update-resume.dto.ts
@@ -1,4 +1,31 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateResumeDto } from './create-resume.dto';
+import { IsArray, IsEmail, IsNotEmpty, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { Types } from 'mongoose';
 
-export class UpdateResumeDto extends PartialType(CreateResumeDto) {}
+class UpdatedBy {
+  @IsNotEmpty()
+  _id: Types.ObjectId;
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+}
+
+class History {
+  @IsNotEmpty()
+  status: string;
+  @IsNotEmpty()
+  updatedAt: Date;
+  @ValidateNested()
+  @IsNotEmpty()
+  @Type(() => UpdatedBy)
+  updatedBy: UpdatedBy;
+}
+export class UpdateResumeDto extends PartialType(CreateResumeDto) {
+  @IsNotEmpty({ message: 'Lịch sử không được để trống!' })
+  @IsArray({ message: 'Lịch sử có định dạng là mảng' })
+  @ValidateNested()
+  @Type(() => History)
+  history: History[];
+}
