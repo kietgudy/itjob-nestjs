7d0d5a02da405751a2686ea27b4536353eb0ca55
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-04 15:31:38 +0700

    Fetch data company with Ref

--- backend-nestjs/src/resumes/resumes.service.ts
+++ backend-nestjs/src/resumes/resumes.service.ts
@@ -43,7 +43,7 @@ export class ResumesService {
   }
 
   async findAll(currentPage: number, limit: number, qs: string) {
-    const { filter, sort, population } = aqp(qs);
+    const { filter, sort, population, projection } = aqp(qs);
     delete filter.current;
     delete filter.pageSize;
     const offset = (+currentPage - 1) * +limit;
@@ -56,6 +56,7 @@ export class ResumesService {
       .limit(defaultLimit)
       .sort(sort as any)
       .populate(population)
+      .select(projection as any)
       .exec();
     return {
       meta: {
--- backend-nestjs/src/resumes/schemas/resume.schema.ts
+++ backend-nestjs/src/resumes/schemas/resume.schema.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
+import { Company } from 'src/companies/schemas/company.schema';
+import { Job } from 'src/jobs/schemas/job.schema';
 export type ResumeDocument = HydratedDocument<Resume>;
 @Schema({ timestamps: true })
 export class Resume {
@@ -11,9 +13,9 @@ export class Resume {
   url: string;
   @Prop()
   status: string;
-  @Prop()
+  @Prop({type: mongoose.Schema.Types.ObjectId, ref: Company.name})
   companyId: mongoose.Schema.Types.ObjectId;
-  @Prop()
+  @Prop({type: mongoose.Schema.Types.ObjectId, ref: Job.name})
   jobId: mongoose.Schema.Types.ObjectId;
   @Prop({ type: mongoose.Schema.Types.Array })
   history: {
