26c5c06f061e6ee0aea16870371166776c5c8b67
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-04 20:33:31 +0700

    create schema roles

--- backend-nestjs/src/roles/schemas/role.schema.ts
+++ backend-nestjs/src/roles/schemas/role.schema.ts
@@ -1,1 +1,40 @@
-export class Role {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Permission } from 'src/permissions/schemas/permission.schema';
+
+export type RoleDocument = HydratedDocument<Role>;
+@Schema({ timestamps: true })
+export class Role {
+  @Prop()
+  name: string;
+  @Prop()
+  description: string;
+  @Prop()
+  isActive: boolean;
+  @Prop({ type: [mongoose.Schema.Types.ObjectId], ref: Permission.name })
+  permissons: Permission[];
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop()
+  createdAt: Date;
+  @Prop()
+  updatedAt: Date;
+  @Prop()
+  isDeleted: boolean;
+  @Prop()
+  deletedAt: Date;
+}
+export const RoleSchema = SchemaFactory.createForClass(Role);
