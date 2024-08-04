f3d6224d66b8869793cadc61e8085412bb9aee69
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-04 20:24:31 +0700

    dto permission

--- backend-nestjs/src/permissions/dto/create-permission.dto.ts
+++ backend-nestjs/src/permissions/dto/create-permission.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePermissionDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreatePermissionDto {
+  @IsNotEmpty({ message: 'Tên không được để trống!' })
+  name: string;
+  @IsNotEmpty({ message: 'Api Path không được để trống!' })
+  apiPath: string;
+  @IsNotEmpty({ message: 'Method không được để trống!' })
+  method: string;
+  @IsNotEmpty({ message: 'Module không được để trống!' })
+  module: string;
+}
