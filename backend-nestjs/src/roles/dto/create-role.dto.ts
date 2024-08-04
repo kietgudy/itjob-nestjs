b3f5854b24841b93bfea44d2aea81a11ed9ff385
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-04 20:41:34 +0700

    dto role

--- backend-nestjs/src/roles/dto/create-role.dto.ts
+++ backend-nestjs/src/roles/dto/create-role.dto.ts
@@ -1,1 +1,16 @@
-export class CreateRoleDto {}
+import { IsArray, IsBoolean, IsMongoId, IsNotEmpty } from 'class-validator';
+import mongoose from 'mongoose';
+
+export class CreateRoleDto {
+  @IsNotEmpty({ message: 'Tên không được để trống!' })
+  name: string;
+  @IsNotEmpty({ message: 'Mô tả không được để trống!' })
+  description: string;
+  @IsNotEmpty({ message: 'isActive không được để trống!' })
+  @IsBoolean({ message: 'isActive có giá trị boolean!' })
+  isActive: boolean;
+  @IsNotEmpty({ message: 'Permissions không được để trống!' })
+  @IsMongoId({each: true, message: "Permissions là object id mongodb"})
+  @IsArray({ message: 'isActive có định dạng là array!' })
+  permissions: mongoose.Schema.Types.ObjectId[];
+}
--- backend-nestjs/src/roles/roles.module.ts
+++ backend-nestjs/src/roles/roles.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Role, RoleSchema } from './schemas/role.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Role.name, schema: RoleSchema }]),
+  ],
   controllers: [RolesController],
-  providers: [RolesService]
+  providers: [RolesService],
 })
 export class RolesModule {}
