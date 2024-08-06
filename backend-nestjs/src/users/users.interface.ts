aeeeed4f9d0f394e867346dd1a0f107a011db2a6
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-06 23:58:18 +0700

    update interface user

--- backend-nestjs/src/users/users.interface.ts
+++ backend-nestjs/src/users/users.interface.ts
@@ -1,6 +1,15 @@
 export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  role: {
     _id: string;
     name: string;
-    email: string;
-    role: string;
-   }
+  };
+  permissions?: {
+    _id: string;
+    name: string;
+    apiPath: string;
+    module: string;
+  }[];
+}
