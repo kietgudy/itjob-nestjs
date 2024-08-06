71cfc0612da65ee5ae1a2848a9bdf9a09746a1ad
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-07 00:09:19 +0700

    update module user

--- backend-nestjs/src/users/users.module.ts
+++ backend-nestjs/src/users/users.module.ts
@@ -3,11 +3,19 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { Role, RoleSchema } from 'src/roles/schemas/role.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      {
+        name: Role.name, schema: RoleSchema,
+      },
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService]
+  exports: [UsersService],
 })
 export class UsersModule {}
--- backend-nestjs/src/users/users.service.ts
+++ backend-nestjs/src/users/users.service.ts
@@ -9,11 +9,14 @@ import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { IUser } from './users.interface';
 import { User } from 'src/decorator/customize';
 import aqp from 'api-query-params';
+import { Role, RoleDocument } from 'src/roles/schemas/role.schema';
+import { USER_ROLE } from 'src/databases/sample';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(UserM.name) private userModel: SoftDeleteModel<UserDocument>,
+    @InjectModel(Role.name) private roleModel: SoftDeleteModel<RoleDocument>,
   ) {}
 
   getHashPassword = (password: string) => {
@@ -54,6 +57,7 @@ export class UsersService {
     if (isExist) {
       throw new BadRequestException(`Email ${email} đã tồn tại`);
     }
+    const userRole = await this.roleModel.findOne({ name: USER_ROLE });
     const hashPassword = this.getHashPassword(password);
     const newRegister = await this.userModel.create({
       name,
@@ -62,7 +66,7 @@ export class UsersService {
       age,
       gender,
       address,
-      role: 'USER',
+      role: userRole?._id,
     });
     return newRegister;
   }
@@ -104,9 +108,11 @@ export class UsersService {
       .populate({ path: 'role', select: { name: 1, _id: 1 } });
   }
   findOneByUsername(username: string) {
-    return this.userModel.findOne({
-      email: username
-    }).populate({ path: 'role', select: { name: 1, permissions: 1 } });
+    return this.userModel
+      .findOne({
+        email: username,
+      })
+      .populate({ path: 'role', select: { name: 1 } });
   }
 
   isValidPassword(password: string, hash: string) {
@@ -156,8 +162,10 @@ export class UsersService {
     );
   };
   findUserByToken = async (refreshToken: string) => {
-    return await this.userModel.findOne({
-      refreshToken,
-    });
+    return await this.userModel
+      .findOne({
+        refreshToken,
+      })
+      .populate({ path: 'role', select: { name: 1 } });
   };
 }
