c8a71bd5bb602d687305affc4ef3e87d7efdb2e5
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-09 16:48:31 +0700

    create subcriber

--- backend-nestjs/src/subscribers/subscribers.controller.ts
+++ backend-nestjs/src/subscribers/subscribers.controller.ts
@@ -2,14 +2,17 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { SubscribersService } from './subscribers.service';
 import { CreateSubscriberDto } from './dto/create-subscriber.dto';
 import { UpdateSubscriberDto } from './dto/update-subscriber.dto';
+import { ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from 'src/users/users.interface';
 
 @Controller('subscribers')
 export class SubscribersController {
   constructor(private readonly subscribersService: SubscribersService) {}
 
   @Post()
-  create(@Body() createSubscriberDto: CreateSubscriberDto) {
-    return this.subscribersService.create(createSubscriberDto);
+  @ResponseMessage("Tạo mới subcriber")
+  create(@Body() createSubscriberDto: CreateSubscriberDto, @User() user:IUser) {
+    return this.subscribersService.create(createSubscriberDto, user);
   }
 
   @Get()
--- backend-nestjs/src/subscribers/subscribers.service.ts
+++ backend-nestjs/src/subscribers/subscribers.service.ts
@@ -1,11 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateSubscriberDto } from './dto/create-subscriber.dto';
 import { UpdateSubscriberDto } from './dto/update-subscriber.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Subscriber, SubscriberDocument } from './schemas/subscriber.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/users/users.interface';
 
 @Injectable()
 export class SubscribersService {
-  create(createSubscriberDto: CreateSubscriberDto) {
-    return 'This action adds a new subscriber';
+  constructor(
+    @InjectModel(Subscriber.name)
+    private subscriberModel: SoftDeleteModel<SubscriberDocument>,
+  ) {}
+
+  async create(createSubscriberDto: CreateSubscriberDto, user: IUser) {
+    const { name, email, skills } = createSubscriberDto;
+    const isExist = await this.subscriberModel.findOne({ email });
+    if (isExist) {
+      throw new BadRequestException(`Email: ${email} đã tồn tại trên hệ thống`);
+    }
+    const newSubs = await this.subscriberModel.create({
+      name,
+      email,
+      skills,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
+    return {
+      _id:newSubs?._id,
+      createdAt: newSubs?.createdAt
+    };
   }
 
   findAll() {
