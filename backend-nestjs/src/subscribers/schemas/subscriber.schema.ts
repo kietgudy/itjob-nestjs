9735725c6d28e1f1fcc221666ae832bfa37745cd
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-09 16:38:19 +0700

    create schema subcriber

--- backend-nestjs/src/subscribers/schemas/subscriber.schema.ts
+++ backend-nestjs/src/subscribers/schemas/subscriber.schema.ts
@@ -1,1 +1,38 @@
-export class Subscriber {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type SubscriberDocument = HydratedDocument<Subscriber>;
+@Schema({ timestamps: true })
+export class Subscriber {
+  @Prop({ required: true })
+  email: string;
+  @Prop()
+  name: string;
+  @Prop()
+  skills: string[];
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop()
+  createdAt: Date;
+  @Prop()
+  updatedAt: Date;
+  @Prop()
+  isDeleted: boolean;
+  @Prop()
+  deletedAt: Date;
+}
+export const SubscriberSchema = SchemaFactory.createForClass(Subscriber);
+
--- backend-nestjs/src/subscribers/subscribers.module.ts
+++ backend-nestjs/src/subscribers/subscribers.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SubscribersService } from './subscribers.service';
 import { SubscribersController } from './subscribers.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Subscriber, SubscriberSchema } from './schemas/subscriber.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{
+    name: Subscriber.name, schema: SubscriberSchema
+  }])],
   controllers: [SubscribersController],
   providers: [SubscribersService]
 })
