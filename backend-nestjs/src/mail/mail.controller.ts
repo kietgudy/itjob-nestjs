0517dd4df67d6be00c86e7225d836f13f9e6c44d
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-08-09 21:48:13 +0700

    controller module email

--- backend-nestjs/src/mail/mail.controller.ts
+++ backend-nestjs/src/mail/mail.controller.ts
@@ -1,9 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { Public, ResponseMessage } from 'src/decorator/customize';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('mail')
 export class MailController {
-  constructor(private readonly mailService: MailService) {
-    
+  constructor(
+    private readonly mailService: MailService,
+    private mailerService: MailerService,
+  ) {}
+  @Get()
+  @Public()
+  @ResponseMessage('Test email')
+  async handleTestEmail() {
+    await this.mailerService.sendMail({
+      to: 'langla76@gmail.com',
+      from: '"Kiet Dev" <support@example.com>', // override default from
+      subject: 'Test Email',
+      html: '<b>welcome test</b>', // HTML body content
+    });
   }
 }
